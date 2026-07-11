872a72415bc8e84e5c1f2df165f77b5db946041b
Luna Midori <118759930+lunamidori5@users.noreply.github.com>
2026-07-10 20:55:13 -0700

    Add Midori AI Radio Studio entrypoint

---
+++ Experimentation/midori-ai-radio-studio/src/main.rs
@@ -0,0 +1,26 @@
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QQuickStyle, QString, QUrl};
+use cxx_qt_lib_extras::QApplication;
+use std::env;
+
+mod backend;
+
+fn main() {
+    let mut app = QApplication::new();
+
+    QGuiApplication::set_desktop_file_name(&QString::from("org.midoriai.RadioStudio"));
+
+    if env::var("QT_QUICK_CONTROLS_STYLE").is_err() {
+        QQuickStyle::set_style(&QString::from("org.kde.desktop"));
+    }
+
+    let mut engine = QQmlApplicationEngine::new();
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from(
+            "qrc:/qt/qml/org/midoriai/radio/src/qml/Main.qml",
+        ));
+    }
+
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
+}
