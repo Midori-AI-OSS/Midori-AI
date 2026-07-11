9f89e319e83e14d30f17ee88ed63fae29f979fe3
Luna Midori <118759930+lunamidori5@users.noreply.github.com>
2026-07-10 20:55:05 -0700

    Add CXX-Qt build configuration

---
+++ Experimentation/midori-ai-radio-studio/build.rs
@@ -0,0 +1,9 @@
+use cxx_qt_build::{CxxQtBuilder, QmlModule};
+
+fn main() {
+    CxxQtBuilder::new_qml_module(
+        QmlModule::new("org.midoriai.radio").qml_file("src/qml/Main.qml"),
+    )
+    .files(["src/backend.rs"])
+    .build();
+}
